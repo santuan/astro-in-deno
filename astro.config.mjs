435e1e70013fb8061dd54b2a39997ddecc1bf180
Sanrodriguez <sanrodriguez@mpf.gov.ar>
2023-06-03 15:32:51 -0300

    e

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: deno(),
+  output: "server",
+  adapter: deno(),
 });
